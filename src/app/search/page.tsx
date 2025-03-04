2315225dc8e6959a51a84e6119a47e1be030e5bd
lflock <lflock03@gmail.com>
2025-03-03 22:34:43 -0800

    Added search ability, results line up and diplay on screen

---
+++ src/app/search/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from 'next/navigation';
+import './search.css';
+
+export default function SearchPage() {
+    const searchParams = useSearchParams();
+    const query = searchParams.get("q") || "";
+
+    const [loading, setLoading] = React.useState(false);
+    const [results, setResults] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+
+    const fetchResults = async (pageNumber: number) => {
+        setLoading(true);
+        try {
+            const response = await fetch(`/api/igdb?q=${query}`);
+            const data = await response.json();
+            setResults(data);
+            console.log(data);
+        } catch (error) {
+            console.error("Failed to fetch search results:", error);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        if (query) {
+            fetchResults(1);
+        }
+    }, [query]);
+
+    useEffect(() => {
+        if (query) {
+            fetchResults(page);
+        }
+    }, [page, query]);
+
+    return (
+        <div className="main-body">
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="search-results">
+                    {results.map((game: any) => (
+                        <div className="result-card" key={game.id}>
+                            <div>
+                                {game.cover && game.cover.url ? (
+                                    <img src={game.cover.url} alt={game.name} width="100" />
+                                ) : (
+                                    <img src="/default-cover.jpg" alt="Default Cover" width="100" />
+                                )}
+                            </div>
+                            <div className="result-info">
+                                <h2>{game.name}</h2>
+                                {/* <p>{game.summary}</p> */}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+            <div className="pagination">
+                <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+                    Previous
+                </button>
+                <span>Page {page}</span>
+                <button onClick={() => setPage(page + 1)}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+};
